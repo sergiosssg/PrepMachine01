ce7f734469ba860e45fb3b5cbab8ba9ee25013bf
sergiosssg <sergiosssg@gmail.com>
2021-06-03 16:29:23 +0300

    LabelOfProcessing changedДобавьте файлы проекта.

---
+++ PrepMachine01/BlackBoard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public class BlackBoard
+    {
+        private IDictionary<bool, ILabelOfPrecessing> labelsSortedByAsProcessed;
+
+
+        public BlackBoard()
+        {
+            labelsSortedByAsProcessed =  new Dictionary<bool, ILabelOfPrecessing>();
+        }
+
+
+        public void addLabel(ILabelOfPrecessing newLabelOfProcessing)
+        {
+
+        }
+
+        public bool containLabel(ILabelOfPrecessing labelOfProcessing)
+        {
+
+            return false;
+        }
+    }
+}
---
+++ PrepMachine01/RulesSystem.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public interface IRule
+    {
+
+    }
+
+    public interface IConditionRule
+    {
+
+    }
+
+    public interface IActionRule
+    {
+
+    }
+
+    public interface ILabelOfPrecessing
+    {
+        public LabelType getThisLabelType();
+        public string getValueAsString();
+        public long getValueAsLong();
+        public int getValueAsInt();
+        public DateTime getValueAsDate();
+        public decimal getValueAsDecimal();
+        public bool getValueAsBool();
+        public bool isLabelActive();
+        public void setAsPassive();
+
+    }
+
+    public enum LabelType
+    {
+        OPERATOR_SOURCE_NAME, 
+        FIRST_PASS_INITIALIZED, 
+        FIRST_PASS_PASSED,
+        SECOND_PASS_INITIALIZED,
+        SECOND_PASS_PASSED,
+        THIRD_INITIALIZED,
+        THIRD_PASSED,
+        PARSING_HEAD_OF_XLSX,
+        PARCING_BODY_TABLE_OF_XLSX,
+        PARCING_HEAD_OF_XLSX_INITIALIZED,
+        PARCING_HEAD_OF_XLSX_PASSED,
+        PARCING_BODY_TABLE_OF_XLSX_INITIALIZED,
+        PARCING_BODY_TABLE_OF_XLSX_PASSED,
+        INVOICE_DATE,
+        RAW_DOCUMENT_NUMBER_ROW,
+        RAW_DOCUMENT_NUMBER_COLUMN,
+        RAW_ROW_SPLITTED_INTO_CELLS
+    }
+
+    public enum ComparisonOperatorType
+    {
+        GT,
+        LT,
+        GE,
+        LE,
+        EQ,
+        NE
+    }
+
+    public enum OperandComparisonType
+    {
+        INT,
+        LONG,
+        DATE,
+        DECIMAL,
+        STRING,
+        BOOL
+    }
+
+
+    public class LabelOfPrecessing : ILabelOfPrecessing
+    {
+        private bool _iAmActive;
+
+        private long _rawLong;
+        private int _rawInt;
+        private string _rawString;
+        private DateTime _rawDateTime;
+        private decimal _rawDecimal;
+        private bool _rawBoolean;
+
+        private LabelType _labelType;
+
+        private OperandComparisonType _operandComparisonType;
+
+
+        public LabelType ThisLabelType  {  set { _labelType = value; }  get{ return _labelType;  } }
+
+
+        public string ValueAsString 
+        { 
+            set 
+            {
+                _operandComparisonType = OperandComparisonType.STRING;
+                _rawString = value; 
+            } 
+            get 
+            { 
+                if (_operandComparisonType == OperandComparisonType.STRING)  return _rawString;
+                return null;
+            } 
+        }
+        public long ValueAsLong 
+        { 
+            set 
+            {
+                _rawString = "" + value;
+                _operandComparisonType = OperandComparisonType.LONG;
+                _rawLong = value;
+            }
+            get 
+            {
+                return _rawLong;
+            }
+        }
+        public int ValueAsInt
+        { 
+            set 
+            {
+                _rawString = "" + value;
+                _operandComparisonType = OperandComparisonType.INT;
+                _rawInt = value;
+            }
+            get 
+            {
+                return _rawInt;
+            }
+        }
+        public DateTime ValueAsDate
+        {
+            set
+            {
+                _rawString = "" + value;
+                _operandComparisonType = OperandComparisonType.DATE;
+                _rawDateTime = value;
+            }
+            get
+            {
+                return _rawDateTime;
+            }
+        }
+        public decimal ValueAsDecimal
+        {
+            set
+            {
+                _rawString = "" + value;
+                _operandComparisonType = OperandComparisonType.DECIMAL;
+                _rawDecimal = value;
+            }
+            get
+            {
+                return _rawDecimal;
+            }
+        }
+        public bool ValueAsBool
+        {
+            set
+            {
+                _rawString = "" + value;
+                _operandComparisonType = OperandComparisonType.BOOL;
+                _rawBoolean = value;
+            }
+            get
+            {
+                return _rawBoolean;
+            }
+        }
+
+
+
+
+
+
+        public bool isLabelActive()  {  return _iAmActive;  }
+
+        public void setAsPassive()  {  _iAmActive = false;  }
+
+        LabelType ILabelOfPrecessing.getThisLabelType()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool ILabelOfPrecessing.getValueAsBool()
+        {
+            throw new NotImplementedException();
+        }
+
+        DateTime ILabelOfPrecessing.getValueAsDate()
+        {
+            throw new NotImplementedException();
+        }
+
+        decimal ILabelOfPrecessing.getValueAsDecimal()
+        {
+            throw new NotImplementedException();
+        }
+
+        int ILabelOfPrecessing.getValueAsInt()
+        {
+            throw new NotImplementedException();
+        }
+
+        long ILabelOfPrecessing.getValueAsLong()
+        {
+            throw new NotImplementedException();
+        }
+
+        string ILabelOfPrecessing.getValueAsString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
