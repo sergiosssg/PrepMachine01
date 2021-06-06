e4263d6e1a171d14c5e51c808868d1561612b109
sergiosssg <sergiosssg@gmail.com>
2021-06-06 09:29:19 +0300

    added in class  BlackBoard  new members: =-  private variable labelsSortedByAsProcessed =- default constructor =- public method addLabel =- public method  containsLabel

--- PrepMachine01/BlackBoard.cs
+++ PrepMachine01/BlackBoard.cs
@@ -6,5 +6,27 @@ namespace PM
 {
     public class BlackBoard
     {
+        private IDictionary<bool, LabelOfProcessing> labelsSortedByAsProcessed;
+
+
+        public BlackBoard()
+        {
+            labelsSortedByAsProcessed =  new Dictionary<bool, LabelOfProcessing>();
+        }
+
+
+        public void addLabel(LabelOfProcessing newLabelOfProcessing)
+        {
+
+        }
+
+        public bool containsLabel(LabelOfProcessing labelOfProcessing)
+        {
+            return false;
+        }
+
+
+
+
     }
 }
