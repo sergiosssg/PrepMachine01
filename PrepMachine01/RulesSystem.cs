b39ca0c7321cb10bdca867ac7798926081110f13
sergiosssg <sergiosssg@gmail.com>
2021-06-07 16:15:34 +0300

    expanded class LabelOfProcessing by two constructors:
    =- by default constructor
    =- by LabelOfProcessing ( LabelType )

--- PrepMachine01/RulesSystem.cs
+++ PrepMachine01/RulesSystem.cs
@@ -38,11 +38,13 @@ namespace PM
         DECIMAL,
         STRING,
         BOOL,
-        FUZZY
+        FUZZY,
+        COMBYLOGIC
     }
 
     public enum FuzzyMeasure
     {
+        DEFINITE_FALSE = 0,
         ZERO = 0,
         TINY = 5,
         WEAK = 10,
@@ -63,9 +65,15 @@ namespace PM
         BIGGEST = 85,
         STRONG = 90,
         HUGE = 95,
-        ABSOLUTE = 100
+        ABSOLUTE = 100,
+        DEFINITE_TRUE = 100
     }
 
+    public struct LogicCombo
+    {
+        public bool booleanLogic;
+        public FuzzyMeasure fuzzyLogic;
+    }
 
 
 
@@ -73,6 +81,7 @@ namespace PM
     public enum LabelType
     {
         OPERATOR_SOURCE_NAME_IN_PROCESSING,
+
         OPERATOR_SOURCE_UKRTELECOM_REVEALED,
         OPERATOR_SOURCE_KIEVSTAR_REVEALED,
         OPERATOR_SOURCE_VODAFON_REVEALED,
@@ -89,7 +98,6 @@ namespace PM
         OPERATOR_SOURCE_KIEVSTAR_PROCESSED_UNSUCCESFULLY,
         OPERATOR_SOURCE_VODAFON_PROCESSED_UNSUCCESFULLY,
 
-
         FIRST_PASS_INITIALIZED,
         FIRST_PASS_PROCESSING,
         FIRST_PASS_PASSED,
@@ -99,13 +107,18 @@ namespace PM
         THIRD_PASS_INITIALIZED,
         THIRD_PASS_PROCESSING,
         THIRD_PASS_PASSED,
+
         PARSING_HEAD_OF_XLSX,
         PARCING_BODY_TABLE_OF_XLSX,
+
         PARCING_HEAD_OF_XLSX_INITIALIZED,
         PARCING_HEAD_OF_XLSX_PASSED,
+
         PARCING_BODY_TABLE_OF_XLSX_INITIALIZED,
         PARCING_BODY_TABLE_OF_XLSX_PASSED,
+
         INVOICE_DATE,
+
         RAW_DOCUMENT_NUMBER_ROW,
         RAW_DOCUMENT_NUMBER_COLUMN,
         RAW_ROW_SPLITTED_INTO_CELLS
@@ -123,6 +136,7 @@ namespace PM
         private DateTime _rawDateTime;
         private decimal _rawDecimal;
         private bool _rawBoolean;
+        private LogicCombo _logicCombo;
 
         private LabelType _labelType;
 
@@ -130,6 +144,25 @@ namespace PM
 
 
         public LabelType ThisLabelType  {  set { _labelType = value; }  get{ return _labelType;  } }
+
+        public LabelOfProcessing()
+        {
+            _iAmActive = true;
+            _rawLong = 0L;
+            _rawInt = 0;
+            _rawString = string.Empty;
+            _rawDateTime = new DateTime(0);
+            _rawDecimal = 0;
+            _rawBoolean = false;
+            _logicCombo.booleanLogic = false;
+            _logicCombo.fuzzyLogic = FuzzyMeasure.DEFINITE_FALSE;
+        }
+
+
+        public LabelOfProcessing(LabelType labelType) : this()
+        {
+            ThisLabelType = labelType;
+        }
 
 
         public string ValueAsString 
