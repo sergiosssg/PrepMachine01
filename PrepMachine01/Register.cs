5a42eda07c39d1a96dd6f44acb4077ba6278564b
sergiosssg <sergiosssg@gmail.com>
2021-06-28 18:25:57 +0300

    changed class Register in production,                     removed properties :        =-   public string OperatorName        =-   public int NumberOfPass        =-   public string TemplateNameOfFilesChainOfInputData        =-   public int NumberOfFileInFilesChainOfInputData        =-   public long NumberOfStringInSourceFileOfInputData        =-   public int NumberOfColumnInSourceFileOfInputData        =-   public string NameOfColumnInSourceFileOfInputData        =-   public OperandType TypeOfData          ///          //// reference for semantic tuple          ///        =-   public byte[] RawData        =-   public string NettoData.

--- PrepMachine01/Register.cs
+++ PrepMachine01/Register.cs
@@ -7,58 +7,5 @@ namespace PM
     public class Register
     {
 
-        public string OperatorName
-        {
-            get; set;
-        }
-
-
-        public int NumberOfPass
-        {
-            get; set;
-        }
-
-        public string TemplateNameOfFilesChainOfInputData
-        {
-            get; set;
-        }
-
-        public int NumberOfFileInFilesChainOfInputData
-        {
-            get; set;
-        }
-
-        public long NumberOfStringInSourceFileOfInputData
-        {
-            get; set;
-        }
-        
-        public int NumberOfColumnInSourceFileOfInputData
-        {
-            get; set;
-        }
-        public string NameOfColumnInSourceFileOfInputData
-        {
-            get; set;
-        }
-
-        public OperandType TypeOfData
-        {
-            get; set;
-        }
-
-        ///
-        //// reference for semantic tuple
-        ///
-
-        public byte[] RawData
-        {
-            get; set;
-        }
-
-        public string NettoData
-        {
-            get; set;
-        }
     }
 }
