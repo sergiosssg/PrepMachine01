1e2a74ae8b37dc195eba757e132cdb26f628d99f
sergiosssg <sergiosssg@gmail.com>
2021-07-01 11:53:42 +0300

    changed  method Equals in class  RegisterRecord  in  production :     so that two instances of RegisterRecord are equal when only,    all current public properties are equal.

--- PrepMachine01/RegisterRecord.cs
+++ PrepMachine01/RegisterRecord.cs
@@ -12,11 +12,29 @@ namespace PM
                 return false;
             }
             RegisterRecord comparedObject = (RegisterRecord)obj;
-            if( this.OperatorName != comparedObject.OperatorName || this.TypeOfData != comparedObject.TypeOfData ||
-                this.NumberOfPass != comparedObject.NumberOfPass )
+            if( !this.OperatorName.Equals( comparedObject.OperatorName) || this.TypeOfData != comparedObject.TypeOfData ||
+                this.NumberOfPass != comparedObject.NumberOfPass)
             {
                 return false;
             }
+            if( this.TemplateNameOfFilesChainOfInputData != string.Empty && comparedObject.TemplateNameOfFilesChainOfInputData != string.Empty &&
+                !this.TemplateNameOfFilesChainOfInputData.Equals( comparedObject.TemplateNameOfFilesChainOfInputData) &&
+                this.NumberOfFileInFilesChainOfInputData != comparedObject.NumberOfFileInFilesChainOfInputData)
+            {
+                return false;
+            }
+            if( this.NumberOfStringInSourceFileOfInputData != comparedObject.NumberOfStringInSourceFileOfInputData &&
+                this.NumberOfColumnInSourceFileOfInputData != comparedObject.NumberOfColumnInSourceFileOfInputData)
+            {
+                return false;
+            }
+            if( this.NameOfColumnInSourceFileOfInputData != string.Empty && comparedObject.NameOfColumnInSourceFileOfInputData != string.Empty &&
+                !this.NameOfColumnInSourceFileOfInputData.Equals( comparedObject.NameOfColumnInSourceFileOfInputData))
+            {
+                return false;
+            }
+
+
             if(( this.NettoData != string.Empty && comparedObject.NettoData != string.Empty ) &&
                  this.NettoData.Equals(comparedObject.NettoData))
             {
