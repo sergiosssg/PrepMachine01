deb42cd9dfa65cc4b88a592a726465704a02a423
sergiosssg <sergiosssg@gmail.com>
2021-07-23 12:05:41 +0300

    added new public interface IStorage, with two public method declarations :     =- public long getHashOfHostMachine()  =- public long getHastOfDateTimeStamp().

---
+++ PrepMachine01/Storages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrepMachine01
+{
+    public interface IStorage
+    {
+        public long getHashOfHostMachine();
+
+        public long getHastOfDateTimeStamp();
+    }
+}
