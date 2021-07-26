87a1da176d652ebaeaacf08a1362c096a9d2111d
sergiosssg <sergiosssg@gmail.com>
2021-07-26 12:17:11 +0300

    added new interface INettodataItem, with one property :  =-  public string NettoData.

---
+++ PrepMachine01/INettodataItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public interface INettodataItem
+    {
+        public string NettoData
+        {
+            get; set;
+        }
+    }
+}
