a52abf03a6877f728f6737d313383fd2ce2e02f7
sergiosssg <sergiosssg@gmail.com>
2021-07-26 12:10:14 +0300

    added new interface IRawdataItem, with one property :  =-  public byte[] RawData.

---
+++ PrepMachine01/IRawdataItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public interface IRawdataItem
+    {
+        public byte[] RawData
+        {
+            get; set;
+        }
+    }
+}
