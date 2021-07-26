58079ffd92d64aabbd30bd6e685aab066a2ab029
sergiosssg <sergiosssg@gmail.com>
2021-07-26 12:01:40 +0300

    added new interface IOperandTypeItem, with one property :  =-  public OperandType TypeOfData.

---
+++ PrepMachine01/IOperandTypeItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public interface IOperandTypeItem
+    {
+        public OperandType TypeOfData
+        {
+            get; set;
+        }
+    }
+}
