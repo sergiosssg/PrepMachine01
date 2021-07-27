f6bab78dbb36f94ac0fe93019a2f862c59f3b79f
sergiosssg <sergiosssg@gmail.com>
2021-07-27 15:16:35 +0300

    added new public interface  IProcessingPassItem  with property :     =-   public int PassNumbes.

---
+++ PrepMachine01/IProcessingPassItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public interface IProcessingPassItem
+    {
+        public int PassNumbes
+        {
+            get; set;
+        }
+    }
+}
