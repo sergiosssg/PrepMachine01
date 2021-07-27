aaf1968f1638d47e44e4ef6e034173b972030ed9
sergiosssg <sergiosssg@gmail.com>
2021-07-27 10:31:18 +0300

    added three new public interfaces :    =-  public interface IRowIndexatorItem with property :        =-   public long RowNumber    =-  public interface IColumnIndexatorItem with properties :        =-   public int ColumnNumber        =-   public string ColumnName    =-  public interface ITableIndicators  inhereted from two interfaces :        =-   IRowIndexatorItem     =-   IColumnIndexatorItem.

---
+++ PrepMachine01/ITableIndicators.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public interface IRowIndexatorItem
+    {
+        public long RowNumber
+        {
+            get; set;
+        }
+    }
+
+    public interface IColumnIndexatorItem
+    {
+        public int ColumnNumber
+        {
+            get; set;
+        }
+
+        public string ColumnName
+        {
+            get; set;
+        }
+    }
+
+    public interface ITableIndicators : IRowIndexatorItem, IColumnIndexatorItem
+    {
+    }
+}
