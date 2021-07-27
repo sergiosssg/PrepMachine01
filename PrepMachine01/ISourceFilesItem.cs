c791f8781d04d9bc7115d14152c06aef30a9f727
sergiosssg <sergiosssg@gmail.com>
2021-07-27 11:35:52 +0300

    added new public interface :        =-   public interface ISourceFilesItem  with property:        =-   public string FileNameTemplate.

---
+++ PrepMachine01/ISourceFilesItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public interface ISourceFilesItem
+    {
+        public string FileNameTemplate
+        {
+            get; set;
+        }
+    }
+}
