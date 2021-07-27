362e8e5c58606d66b99c9af3636e269ac6d3a262
sergiosssg <sergiosssg@gmail.com>
2021-07-27 12:46:33 +0300

    added new public interface IOperatorItem with property :     =-   public string OperatorName.

---
+++ PrepMachine01/Operator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public interface IOperatorItem
+    {
+        public string OperatorName
+        {
+            get; set;
+        }
+    }
+}
