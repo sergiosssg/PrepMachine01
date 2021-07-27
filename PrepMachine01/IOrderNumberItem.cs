6f6757b7fafb449d11bf960cda3159ea26855948
sergiosssg <sergiosssg@gmail.com>
2021-07-27 11:50:37 +0300

    added new public interface :    =-  public interface IOrderNumberItem :        =-   public int OrderNumberInChain.

---
+++ PrepMachine01/IOrderNumberItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public interface IOrderNumberItem
+    {
+        public int OrderNumberInChain
+        {
+            get; set;
+        }
+    }
+}
