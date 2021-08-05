d287fef8ff5277ac8e55e85709509069449abc03
sergiosssg <sergiosssg@gmail.com>
2021-08-05 15:54:40 +0300

    created  new  public  interface   ILabelItem  with  two  public  methods :     =-  public   LabelType LabelTypeProperty     =-  public   LabelOfProcessing LabelOfProcessingProperty.

---
+++ PrepMachine01/ILabelItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public interface ILabelItem
+    {
+        public LabelType LabelTypeProperty
+        {
+            get;
+            set;
+        }
+
+        public LabelOfProcessing LabelOfProcessingProperty
+        {
+            get;
+            set;
+        }
+    }
+}
