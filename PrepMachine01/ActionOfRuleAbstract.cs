1f3de9f3105fcc37a1fe0e900426897b3d70c456
sergiosssg <sergiosssg@gmail.com>
2021-08-05 15:27:12 +0300

    created  new   public  abstract class  ActionOfRuleAbstract,  which  implements  interface  IActionOfRule,  with  two abstract member functions  :       =-   public  abstract  bool    DoAction()       =-  public  abstract   ActionOfRuleType   GetActionOfRuleType().

---
+++ PrepMachine01/ActionOfRuleAbstract.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public abstract class ActionOfRuleAbstract : IActionOfRule
+    {
+        public abstract bool DoAction();
+
+
+        public abstract ActionOfRuleType GetActionOfRuleType();
+
+    }
+}
