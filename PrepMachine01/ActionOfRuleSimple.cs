2dc75d2bcd60e06d236cab60922b406317b7443b
sergiosssg <sergiosssg@gmail.com>
2021-08-06 15:56:21 +0300

    Added new public class ActionOfRuleSimple,  extended  abstract class ActionOfRuleAbstract,                                    wit  two methods :      =-  public override bool DoAction()      =-  public override ActionOfRuleType GetActionOfRuleType().

---
+++ PrepMachine01/ActionOfRuleSimple.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public class ActionOfRuleSimple : ActionOfRuleAbstract
+    {
+        public override bool DoAction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ActionOfRuleType GetActionOfRuleType()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
