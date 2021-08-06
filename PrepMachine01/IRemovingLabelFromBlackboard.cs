f037eb712f1ad6204e633938610954ccbdcf23b2
sergiosssg <sergiosssg@gmail.com>
2021-08-06 12:02:42 +0300

    Created  new  public  interface  IRemovingLabelFromBlackboard,  with  one  public  method :     =-  bool RemoveLabelFromBlackboard(BlackBoard blackBoard, LabelOfProcessing label).

---
+++ PrepMachine01/IRemovingLabelFromBlackboard.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public interface IRemovingLabelFromBlackboard
+    {
+        bool RemoveLabelFromBlackboard(BlackBoard blackBoard, LabelOfProcessing label);
+    }
+}
