16a1618dfe64c3eb261e0f818c7df57dbb00f9dc
sergiosssg <sergiosssg@gmail.com>
2021-08-06 15:17:09 +0300

    Created  new  public  class  IRemovingRecordFromRegister,                    extended  abstract  class ActionOfRuleAbstract  and        implement  interfaces  IAddingLabelToBlackboard  and  IRemovingLabelFromBlackboard,        with four methods :
        =-  public override bool DoAction()
        =-  public override ActionOfRuleType GetActionOfRuleType()
        =-  public bool AddLabelToBlackboard(BlackBoard blackBoard, LabelOfProcessing label)
        =-  public bool RemoveLabelFromBlackboard(BlackBoard blackBoard, LabelOfProcessing label).

---
+++ PrepMachine01/ActionOfRuleForBlackboard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public class ActionOfRuleForBlackboard : ActionOfRuleAbstract, IAddingLabelToBlackboard, IRemovingLabelFromBlackboard
+    {
+        public override bool DoAction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ActionOfRuleType GetActionOfRuleType()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public bool AddLabelToBlackboard(BlackBoard blackBoard, LabelOfProcessing label)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveLabelFromBlackboard(BlackBoard blackBoard, LabelOfProcessing label)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
