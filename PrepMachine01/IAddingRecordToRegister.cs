b9e7733fef8e8119ca1110980e7c1e0926ca301b
sergiosssg <sergiosssg@gmail.com>
2021-08-06 12:17:20 +0300

    Created  new  public  interface  IAddingRecordToRegister,  with  one  public  method :     =-  bool AddRecordToRegister(Register register, IRecord record).

---
+++ PrepMachine01/IAddingRecordToRegister.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public interface IAddingRecordToRegister
+    {
+        bool AddRecordToRegister(Register register, IRecord record);
+    }
+}
