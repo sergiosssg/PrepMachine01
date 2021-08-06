360f18dcec57deec1178c70250245b3b01b6379a
sergiosssg <sergiosssg@gmail.com>
2021-08-06 11:50:25 +0300

    Created  new  public  interface  IAddingLabelToBlackboard,  with  one  public  method :     =-  public bool AddLabelToBlackboard(BlackBoard blackBoard, LabelOfProcessing label).

---
+++ PrepMachine01/IAddingLabelToBlackboard.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public interface IAddingLabelToBlackboard
+    {
+        public bool AddLabelToBlackboard(BlackBoard blackBoard, LabelOfProcessing label);
+    }
+}
