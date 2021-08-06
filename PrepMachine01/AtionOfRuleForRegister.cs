33dadeec7526c469392ff151bab2d882c392e559
sergiosssg <sergiosssg@gmail.com>
2021-08-06 15:34:25 +0300

    Created  new  public  class  AtionOfRuleForRegister,                    extended  abstract  class ActionOfRuleAbstract  and        implement  interfaces  IAddingRecordToRegister  and  IRemovingRecordFromRegister,        with four methods :
        =-  public override bool DoAction()
        =-  public override ActionOfRuleType GetActionOfRuleType()
        =-  public bool AddRecordToRegister(Register register, IRecord record)
        =-  public bool RemoveRecordFromRegister(Register register, IRecord record).

---
+++ PrepMachine01/AtionOfRuleForRegister.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public class AtionOfRuleForRegister : ActionOfRuleAbstract, IAddingRecordToRegister, IRemovingRecordFromRegister
+    {
+        public override bool DoAction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ActionOfRuleType GetActionOfRuleType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddRecordToRegister(Register register, IRecord record)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveRecordFromRegister(Register register, IRecord record)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
