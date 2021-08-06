e3530f769a51edb45a4b62db5cfed48569bf58f1
sergiosssg <sergiosssg@gmail.com>
2021-08-06 10:12:01 +0300

    Added new public interface  IActionOfRuleTypeItem with one method :     =-    public abstract ActionOfRuleType GetActionOfRuleType().

---
+++ PrepMachine01/IActionOfRuleTypeItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public interface IActionOfRuleTypeItem
+    {
+        public abstract ActionOfRuleType GetActionOfRuleType();
+    }
+}
