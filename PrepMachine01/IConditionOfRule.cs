cf56620228989554ba84a83eed5b4a9d0828cf9e
sergiosssg <sergiosssg@gmail.com>
2021-08-08 19:13:52 +0300

    Changed  interface IConditionOfRule, by means               exchanging methods to properties :  =- ComparisonOperatorType getComparisonOperatorType   -->  ComparisonOperatorType  =- OperandType getOperandType()                                         -->  OperandTypeProperty  =- LogicCombo getLogicCombo()                                           -->  LogicComboProperty  =- FuzzyMeasure getЕhresholdLevel()                                      -->  FuzzyLevel  =- ValueLocationType GetValueLocationType()                        -->  ValueLocationTypeProperty.

--- PrepMachine01/IConditionOfRule.cs
+++ PrepMachine01/IConditionOfRule.cs
@@ -2,11 +2,31 @@
 {
     public interface IConditionOfRule
     {
-        public ComparisonOperatorType getComparisonOperatorType();
-        public OperandType getOperandType();
-        public LogicCombo getLogicCombo();
-        public FuzzyMeasure getЕhresholdLevel();
-        public ValueLocationType GetValueLocationType();
+        public ComparisonOperatorType ComparisonOperatorType
+        {
+            get;
+            set;
+        }
+        public OperandType OperandTypeProperty 
+        {
+            get;
+            set;
+        }
+        public LogicCombo LogicComboProperty
+        {
+            get;
+            set;
+        }
+        public FuzzyMeasure FuzzyLevel
+        {
+            get;
+            set;
+        }
+        public ValueLocationType ValueLocationTypeProperty 
+        {
+            get;
+            set;
+        }
         public System.Predicate<LabelOfProcessing> PredicateForComparision
         {
             get;
