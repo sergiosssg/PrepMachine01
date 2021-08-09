b77fa7ae90b3c98df2af47b024ec8b78b121ed5d
sergiosssg <sergiosssg@gmail.com>
2021-08-09 15:55:03 +0300

    Added  new  public  class   OperandValueOfProcessing,  implementing  interface  IValueOfProcessing,                                                                                                                                           with  public  properties :     =-     public OperandType ComparisonOperandType     =-     public string NettoData     =-     public string OperatorName     =-     public int PassNumbes .

---
+++ PrepMachine01/OperandValueOfProcessing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public class OperandValueOfProcessing : IValueOfProcessing
+    {
+        public OperandType ComparisonOperandType 
+        { 
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException(); 
+        }
+        public string NettoData 
+        { 
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException(); 
+        }
+        public string OperatorName 
+        { 
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException(); 
+        }
+        public int PassNumbes 
+        { 
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException(); 
+        }
+    }
+}
