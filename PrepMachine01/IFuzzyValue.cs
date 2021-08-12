0cab968629076e6619151c253cfd5426085c1014
sergiosssg <sergiosssg@gmail.com>
2021-08-12 10:55:36 +0300

    Added new public interface  IFuzzyValue,  with  one public  property :     =-  public FuzzyMeasure FuzzyProperty.

---
+++ PrepMachine01/IFuzzyValue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public interface IFuzzyValue
+    {
+        public FuzzyMeasure FuzzyProperty
+        {
+            get;
+            set;
+        }
+    }
+}
