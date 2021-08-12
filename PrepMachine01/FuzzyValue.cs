d9ace87a80f8abb26cd4e8bc6f0d381be67e689f
sergiosssg <sergiosssg@gmail.com>
2021-08-12 11:02:21 +0300

    Added new public class   FuzzyValue,  implementing  IFuzzyValue.

---
+++ PrepMachine01/FuzzyValue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public class FuzzyValue : IFuzzyValue
+    {
+        public FuzzyMeasure FuzzyProperty { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
