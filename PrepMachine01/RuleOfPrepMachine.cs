3a6f33d431a1488208c89f621e228be929c2faec
sergiosssg <sergiosssg@gmail.com>
2021-08-19 12:39:20 +0300

    Created  new  class RuleOfPrepMachine  extented  RuleAbstract.

---
+++ PrepMachine01/RuleOfPrepMachine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public class RuleOfPrepMachine : RuleAbstract
+    {
+        public override IActionOfRule[] GetActionsOfRule()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IConditionOfRule[] GetConditionsOfRule()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsRuleActive()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
