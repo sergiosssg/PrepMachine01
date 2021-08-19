29394c657a21b5802347c22e76e558eb12edab6a
sergiosssg <sergiosssg@gmail.com>
2021-08-19 15:41:00 +0300

    Created  new  class  StorageOfRules  implements IStorage.

---
+++ PrepMachine01/StorageOfRules.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public class StorageOfRules : IStorage
+    {
+        public int Amount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Load(string whereFrom)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Loaded()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Store(string where)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Stored()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
