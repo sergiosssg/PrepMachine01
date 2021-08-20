2f104d5a145c732c95bcca7e8d228f4de6b1107b
sergiosssg <sergiosssg@gmail.com>
2021-08-20 12:07:18 +0300

    Changed class  BlackBoard, so now it implements IStorage.

--- PrepMachine01/BlackBoard.cs
+++ PrepMachine01/BlackBoard.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace PM
 {
-    public class BlackBoard 
+    public class BlackBoard : IStorage<IValueOfProcessing, string>
     {
         private IDictionary<bool, IValueOfProcessing> _labelsSortedByAsProcessed;
         private ISet<IValueOfProcessing> _setOfLabels;
@@ -16,9 +16,37 @@ namespace PM
             _setOfLabels = new HashSet<IValueOfProcessing>();
         }
 
+        public bool Add(IValueOfProcessing tElement)
+        {
+            _labelsSortedByAsProcessed.Add( false, tElement);
+            _setOfLabels.Add( tElement);
+            return true;
+        }
 
+        public bool Contains(IValueOfProcessing tElement)
+        {
+            throw new NotImplementedException();
+        }
 
+        public bool Load(string whereFrom)
+        {
+            throw new NotImplementedException();
+        }
 
+        public bool Loaded()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Store(string where)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Stored()
+        {
+            throw new NotImplementedException();
+        }
 
 
 
@@ -31,18 +59,9 @@ namespace PM
         /// </summary>
         /// <param name="newLabelOfProcessing"></param>
 
-        public void addLabel(IValueOfProcessing newLabelOfProcessing)
+        public int Amount()
         {
-            if (newLabelOfProcessing != null)
-            {
-                _labelsSortedByAsProcessed.Add(false, newLabelOfProcessing);
-                _setOfLabels.Add(newLabelOfProcessing);
-            }
-        }
-
-        public bool containsLabel(IValueOfProcessing labelOfProcessing)
-        {
-            return false;
+            throw new NotImplementedException();
         }
     }
 }
