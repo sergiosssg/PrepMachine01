0b479efc6d6fd3956879b585a3267175d1ed6a2f
sergiosssg <sergiosssg@gmail.com>
2021-08-25 15:51:33 +0300

    Added new public interface parameterized  IValueItem<V>, with public methods :         =-   public V getValue()         =-   public bool setValue(V val)         =-   public OperandType getType()         =-   public bool AssignedValue().

---
+++ PrepMachine01/IValueItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM
+{
+    public interface IValueItem<V>
+    {
+        public V getValue();
+        public bool setValue(V val);
+
+        public OperandType getType();
+        public bool AssignedValue();
+    }
+}
